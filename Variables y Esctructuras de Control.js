228defbf683409cc044de50aa473319bd4a70765
Jose Quiñónez <joserobertoq@outlook.com>
2022-02-05 17:31:01 -0600

    initial commit

---
+++ Variables y Esctructuras de Control.js
@@ -0,0 +1,15 @@
+let mes = 10;
+
+if (mes === 12 || (mes >= 1 && mes <= 2)) {  
+    console.log("es inverno");
+} else if (mes > 3 && mes <= 5) {
+    console.log("es primavera");
+} else if(mes > 5 && mes <= 8) {
+    console.log("es verano");
+} else if(mes > 8 && mes <= 11) {
+    console.log("es otoño");
+} else {
+    console.log("mes inválido");
+}
+
+console.log("Número de carné: 22002759 - Nombre completo: José Roberto Quiñónez Girón");
